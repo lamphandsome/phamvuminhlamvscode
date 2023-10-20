885ccaa9bca17a3d2b1f384f6b811fcaaa65ff49
lamphandsome <lamphamxxx04@gmail.com>
2023-10-20 09:36:56 +0700

    .

--- js4/js/for-loop.js
+++ js4/js/for-loop.js
@@ -1,10 +1,10 @@
-var scores = [24, 32, 17];
+var scores = [24, 32, 17, 16, 86];
 var arrayLength = scores.length;
 var roundNumber = 0;
 var msg ='';
 for(var i =0; i < arrayLength;i++){
     roundNumber = (i+1);
-msg += 'round' + roundNumber + ':'
+msg += 'round ' + roundNumber + ': '
 msg += scores[i] + '<br />';
 }
 document.getElementById('answer').innerHTML = msg;
--- js4/js/while-loop.js
+++ js4/js/while-loop.js
@@ -1,6 +1,6 @@
 var i = 7;
 var msg = '';
-while(i<10){
+while(i<15){
     msg+= i + 'x 5 = ' + (i*5) + '<br />';
     i++;
 }
